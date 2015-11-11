df5d95f6b4ea9a64b058599ace445313837de7c6
PimVanAlphen <paalphen@hotmail.com>
2015-11-11 16:01:38 +0100

    lijst van directions in de carslist

--- Gezamenlijk/CarsList.py
+++ Gezamenlijk/CarsList.py
@@ -2,9 +2,15 @@
 class CarsList:
     def __init__(self):
         self.cars = []
+        self.directionsList = []
 
     def getFirstState(self):
     	firstState = []
     	for car in self.cars:
     		firstState.append(car.firstMainTile)
     	return firstState
+
+    def getDirectionsList(self):
+        for cars in self.cars:
+            self.directionsList.append(cars.isHorizontal)
+        return self.directionsList
