4231e9bdb88719870d958cea626ed7695f63429e
Alex <a.meerdervan@gmail.com>
2015-11-13 15:28:27 +0100

    HIJ DOET HET! het eerste resultaat voor 6 bij 6, geeft 3100 solutions

--- Gezamenlijk/CarsList.py
+++ Gezamenlijk/CarsList.py
@@ -5,23 +5,19 @@ class CarsList:
         self.directionsList = []
 
     def getFirstState(self):
-<<<<<<< HEAD
         firstState = []
         for car in self.cars:
             firstState.append(car.firstMainTile)
         return firstState
-=======
     	firstState = []
     	for car in self.cars:
     		firstState.append(car.firstMainTile)
     	return firstState
->>>>>>> df5d95f6b4ea9a64b058599ace445313837de7c6
 
     def getDirectionsList(self):
         for cars in self.cars:
             self.directionsList.append(cars.isHorizontal)
         return self.directionsList
-<<<<<<< HEAD
         
     def getVisualisationList(self):
         visualisationList = []
@@ -32,5 +28,3 @@ class CarsList:
             tmpList.append(car.length)
             visualisationList.append(tmpList)
         return visualisationList
-=======
->>>>>>> df5d95f6b4ea9a64b058599ace445313837de7c6
